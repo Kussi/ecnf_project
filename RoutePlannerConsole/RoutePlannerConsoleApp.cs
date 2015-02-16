059222c2f4e1e7099d33909b5ca86f505b7a89c4
Kussi <markus.recher@students.fhnw.ch>
2015-02-16 18:16:11 +0100

    initial commit

---
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            int nr = 1;
+            Console.WriteLine("Welcome to RoutePlanner (Version "+ nr + ")");
+            Console.ReadLine();
+        }
+    }
+}
