c40a9669f65797531cdf2af21d58717234cc6cb1
Kussi <markus.recher@students.fhnw.ch>
2015-02-17 08:24:42 +0100

    Lab1 finalized

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
 {
@@ -10,8 +12,12 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
     {
         static void Main(string[] args)
         {
-            int nr = 1;
-            Console.WriteLine("Welcome to RoutePlanner (Version "+ nr + ")");
+            Version version = Assembly.GetExecutingAssembly().GetName().Version;
+            Console.WriteLine("Welcome to RoutePlanner (Version " + version + ")");
+
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+
             Console.ReadLine();
         }
     }
