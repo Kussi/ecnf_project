6f7a3827a82de15b4918d7d7cba0c94c01834aef
Kussi <markus.recher@students.fhnw.ch>
2015-02-23 14:21:55 +0100

    Lab2  task1 implemented

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -16,7 +16,11 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
             Console.WriteLine("Welcome to RoutePlanner (Version " + version + ")");
 
             var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
-            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+            var bern = new WayPoint("Bern", 46.9546699, 7.39487);
+            var tripolis = new WayPoint("Tripolis", 32.8255927, 13.1133419);
+            //Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+
+            Console.WriteLine(tripolis.Distance(bern));
 
             Console.ReadLine();
         }
