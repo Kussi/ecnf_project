bfc06d24524c0c7cafb48909e7f0e3f4a7c82c37
Kussi <markus.recher@students.fhnw.ch>
2015-02-24 10:48:50 +0100

    Revert "beautification of the code"
    This reverts commit 34a013e1b4d152d2cdbd76cd14682f5bf7921f32.

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -13,21 +13,26 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
     {
         static void Main(string[] args)
         {
-            // version
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
             Console.WriteLine("Welcome to RoutePlanner (Version " + version + ")");
-            Console.WriteLine("=========================================");
-            Console.WriteLine();
 
-            // testcode
             var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
             var bern = new WayPoint("Bern", 46.9546699, 7.39487);
             var tripolis = new WayPoint("Tripolis", 32.8255927, 13.1133419);
+            //Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
 
-            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+            Cities cs = new Cities();
+            City c1 = new City("Möhlin", "CH", 10, 10.1, 11.12);
+
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Console.WriteLine(cs.ReadCities("citiesTestDataLab2.txt"));
+
+            Console.WriteLine(cs.Count);
+
 
             Console.WriteLine();
-            Console.WriteLine("distance between tripolis and bern:");
+
+
             Console.WriteLine(tripolis.Distance(bern));
 
             Console.ReadLine();
